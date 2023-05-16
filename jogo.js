0fce4a6d9702e49d4f8e42f879299c5439a9eb14
Renato Motikawa <renatomtk@MacBook-Air-de-Renato.local>
2023-05-16 17:52:34 -0300

    first commit

--- jogo.js
+++ jogo.js
@@ -3,6 +3,23 @@ var largura = 0
 var vidas = 1
 var tempo = 15
 
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+    criaMosquitoTempo = 1500
+}
+
+else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000
+}
+
+    else if(nivel === 'hardcore') {
+    criaMosquitoTempo = 750
+}
+
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
@@ -19,7 +36,7 @@ var cronometro = setInterval(function() {
     if(tempo < 0) {
         clearInterval(cronometro)
         clearInterval(criaMosquito)
-        alert('vitoria')
+        window.location.href = 'vitoria.html'
     }
 
     else {
